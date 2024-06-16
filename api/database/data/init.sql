d8783e7a799acdcae5dc6c870ac29e0f12b49db3
Sebastian Rühmann <mail@sebastianruehmann.de>
2024-06-16 23:54:07 +0200

    Publish api

--- api/database/data/init.sql
+++ api/database/data/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (
+CREATE TABLE project (
     id SERIAL PRIMARY KEY,                          -- Unique identifier for each project
     name VARCHAR(255) NOT NULL,                     -- Name of the project
     country VARCHAR(100),                           -- Country where the project is located
@@ -11,5 +11,5 @@ CREATE TABLE projects (
     description TEXT                                -- Description of the project
 );
 
-COPY projects(id, name, country, image, price_per_ton, offered_volume_in_tons, distribution_weight, supplier_name, earliest_delivery, description)
+COPY project(id, name, country, image, price_per_ton, offered_volume_in_tons, distribution_weight, supplier_name, earliest_delivery, description)
 FROM '/docker-entrypoint-initdb.d/projects.csv' DELIMITER ',' CSV HEADER;
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+      }),
+    }),
+    ProjectsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
